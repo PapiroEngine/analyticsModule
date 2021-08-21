5febb2ad2ad4350bb80354502d19d719ebbdca29
jgpampanin <78567492+jgpampanin@users.noreply.github.com>
2021-08-21 13:33:07 -0300

    Questionnaires API
    First version of the questionnaires API for tests. Still in development

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 from dash_app import create_dash_app
-
+from flask_restful import Resource, Api
+import pandas as pd
+import pymongo
+from bson.json_util import dumps
 
 app = Flask(__name__)
+api = Api(app)
 
 create_dash_app(app)
 
@@ -27,3 +31,22 @@ def thankyou():
 	email = request.form.get("testeemail")
 	senha = request.form.get("senha")
 	return render_template("thankyou.html", email=email, senha=senha)
+
+
+#General DB path
+myclient = pymongo.MongoClient("mongodb+srv://admin:12209005@main-learninganalyticsc.kgwfb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", ssl=True,ssl_cert_reqs='CERT_NONE')
+mydb = myclient["Papiro_Statistics_DB"]
+mycol = mydb["Questionnaire_Templates"]
+
+auth_token = "LDhkmZP2tkXBTmrB4TNjKQQtXftJBJT337YZVumerK4ensx6Z4afxLy3kuQPJZGFEqW7jnLNYJFYKefbWUhp24MtzGa5T2fDg3Nvnp3DfPXhc27cW7kXZQ3SpJ2XGMxv"
+
+class Teste(Resource):
+    def get(self, auth, questType):
+        if auth != auth_token:
+            return false
+        else:
+            a = mycol.find({"QType": questType },{"_id": 0})
+            b = list(a)
+            return dumps(b)
+
+api.add_resource(Teste, '/questionnaires/templates/<auth>/<questType>')
